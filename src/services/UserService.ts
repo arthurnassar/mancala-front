83474055dd4152786cb191967046f492e3c2eec5
arthurnassar <arthurnassar@gmail.com>
2022-12-11 15:52:17 +0000

    Refactor userService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,46 +1,43 @@
 import { useUserStore } from '@/stores/userStore'
-import {
-  type UserResponseData,
-  type CreateUserData,
-API_GET_ALL_USERS,
-API_CREATE_USER
-} from '@/types/UserTypes'
+import { type UserResponseData, type CreateUserData, API_GET_ALL_USERS, API_CREATE_USER, GetMethod } from '@/types/UserTypes'
 import axios, { type AxiosInstance, type AxiosResponse } from 'axios'
 
-// INSTANCE CREATION
-const userInstance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
-  timeout: 3000
-})
-//
+class UserService {
+  private baseURL: string
+  private headers: { [key: string]: string}
+  private instance: AxiosInstance
 
-// GET AND DELETE REQUEST INTERCEPTOR
-// :: INJECT BEARER TOKEN ::
-userInstance.interceptors.request.use((config) => {
-  const userStore = useUserStore()
+  constructor(baseURL: string) {
+    this.baseURL = baseURL,
+    this.headers = { 'Content-Type': 'application/json' },
+    this.instance = axios.create({
+      baseURL: this.baseURL,
+      headers: this.headers
+    })
 
-  const configEqualsGet = config.method === 'get' || config.method === 'GET'
-
-  if (
-    (configEqualsGet) &&
-    !!userStore.bearerToken
-  ) {
-    config.headers = {
-      Authorization: 'Bearer ' + userStore.bearerToken
-    }
+    this.instance.interceptors.request.use((config) => {
+      const userStore = useUserStore()
+      
+      const configEqualsGet = config.method === GetMethod.GET || config.method === GetMethod.Get || config.method === GetMethod.get
+      const methodIsGetAndBearerTokenExists = (configEqualsGet) && !!userStore.bearerToken
+      if (methodIsGetAndBearerTokenExists) {
+        config.headers = {
+          Authorization: 'Bearer ' + userStore.bearerToken
+        }
+      }
+    
+      return config
+    })
   }
 
-  return config
-})
-//
-
-// USERS INSTANCE METHODS
-const fetchUsers = (): Promise<AxiosResponse<UserResponseData[], any>> => {
-  return userInstance.get(API_GET_ALL_USERS)
+  fetchUsers(): Promise<AxiosResponse<UserResponseData[], any>> {
+    return this.instance.get(API_GET_ALL_USERS)
+  }
+  
+  createUser(data: CreateUserData): Promise<AxiosResponse> {
+    return this.instance.post(API_CREATE_USER, data)
+  }
 }
 
-const createUser = (data: CreateUserData): Promise<AxiosResponse> => {
-  return userInstance.post(API_CREATE_USER, data)
-}
-
-export { fetchUsers, createUser }
+const apiBaseUrl = import.meta.env.PROD ? import.meta.env.VITE_API_BASE_URL : 'http://localhost:3000/'
+export const userService = new UserService(apiBaseUrl)
