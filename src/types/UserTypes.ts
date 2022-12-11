ebe13f5cb76a6ad0457ef564d48d7fa22705f3d4
arthurnassar <arthurnassar@gmail.com>
2022-12-11 15:52:35 +0000

    Create userService enum

--- src/types/UserTypes.ts
+++ src/types/UserTypes.ts
@@ -17,3 +17,9 @@ export const BEARER_TOKEN = 'bearer_token'
 export const IS_LOGGED_IN = 'is_logged_in'
 export const API_CREATE_USER = 'user'
 export const API_GET_ALL_USERS = 'userS'
+
+export enum GetMethod {
+  get = "get",
+  GET = "GET",
+  Get = "Get"
+}
